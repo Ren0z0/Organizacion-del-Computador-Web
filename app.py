fde49f5d6e9d675c0f89023d12b8b72da1ba1edb
Ren0z0 <renzosantiagomartin@outlook.es>
2025-03-20 16:40:57 -0300

    nav_desktop.html : agrego for para la laista

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def Home():
+    lista = ["Home", "Docentes", "Calendario", "Informacion"]
+    return render_template("home.html", lista=lista)
+
+@app.route("/")
+def Docentes():
     return render_template("home.html")
+
+@app.route("/")
+def Calendario():
+    return render_template("home.html")
+
+@app.route("/")
+def Informacion():
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5001,debug=True)
