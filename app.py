5b87e06bf995f3d0c23b91cbced60ec51fe95446
Leonel R. <leorhcp2030@gmail.com>
2025-03-20 11:56:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'hola mundo'
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5001,debug=True)
