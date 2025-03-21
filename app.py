7f0ef2992b9f3466671e4affca67e7fbe7b25a9b
Leonel R. <leorhcp2030@gmail.com>
2025-03-21 01:20:26 -0300

    agrego links y fotos de los profes

--- app.py
+++ app.py
@@ -3,22 +3,8 @@ from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
-    lista = ["Home", "Docentes", "Calendario", "Informacion"]
-    return render_template("home.html", lista=lista)
-
-@app.route("/")
-def Docentes():
+def home():
     return render_template("home.html")
-
-@app.route("/")
-def Calendario():
-    return render_template("home.html")
-
-@app.route("/")
-def Informacion():
-    return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5001,debug=True)
